c0fd54705700c1f6c29f55bb5f47421eed709b08
Takayuki Muranushi <muranushi@gmail.com>
2013-06-05 09:53:13 +0900

    generate features in parallel.

--- run-par.sh
+++ run-par.sh
@@ -1,8 +1,8 @@
-./dist/build/main/main &
-./dist/build/main/main -p=1 &
-./dist/build/main/main -p=2 &
-./dist/build/main/main -p=3 &
-./dist/build/main/main -u &
-./dist/build/main/main -u -p=1 &
-./dist/build/main/main -u -p=2 &
-./dist/build/main/main -u -p=3 &
+./dist/build/main/main feature &
+./dist/build/main/main -p=1 feature &
+./dist/build/main/main -p=2 feature &
+./dist/build/main/main -p=3 feature &
+./dist/build/main/main -u feature &
+./dist/build/main/main -u -p=1 feature &
+./dist/build/main/main -u -p=2 feature &
+./dist/build/main/main -u -p=3 feature &
