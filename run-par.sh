28fd791c83aff3c16fd3c4e99fed290b40e10b17
nushio <nushio@nushiohex.kuins.net>
2013-06-05 09:25:42 +0900

    runnign script!

---
+++ run-par.sh
@@ -0,0 +1,8 @@
+./dist/build/main/main &
+./dist/build/main/main -p=1 &
+./dist/build/main/main -p=2 &
+./dist/build/main/main -p=3 &
+./dist/build/main/main -u &
+./dist/build/main/main -u -p=1 &
+./dist/build/main/main -u -p=2 &
+./dist/build/main/main -u -p=3 &
