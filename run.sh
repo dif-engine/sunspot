e9d09dd3149cbbf0e0b46b5db5a105b1b7ff6b8f
nushio <nushio@nushiohex.kuins.net>
2013-06-05 09:24:24 +0900

    fix running script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 make
-./dist/build/main/main
+./dist/build/main/main -o feature
 cat feature/20120123_hmi.txt feature/20120307_hmi.txt > training.txt
 ./libsvm/easy.py ./training.txt ./feature/20130515_11745_hmi.txt
